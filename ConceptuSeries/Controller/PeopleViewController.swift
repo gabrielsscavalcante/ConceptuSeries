0c101bbca52e28c4d3a99c56c2f61f4bbbbb1d72
gabrielsscavalcante <gabrielsoucavalcante@gmail.com>
2018-03-17 18:58:46 -0300

    Add PeopleViewController

---
+++ ConceptuSeries/Controller/PeopleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PeopleViewController.swift
+//  ConceptuSeries
+//
+//  Created by Gabriel Cavalcante on 17/03/2018.
+//  Copyright © 2018 Gabriel Cavalcante. All rights reserved.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

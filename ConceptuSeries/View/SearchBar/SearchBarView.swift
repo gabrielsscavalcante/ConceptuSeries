1a3b02f886d6218ebd3b6b22b9c4711a1fd10ac6
gabrielsscavalcante <gabrielsoucavalcante@gmail.com>
2018-03-17 19:17:41 -0300

    Add some icons and init search bar view.

---
+++ ConceptuSeries/View/SearchBar/SearchBarView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchBarView.swift
+//  ConceptuSeries
+//
+//  Created by Gabriel Cavalcante on 17/03/2018.
+//  Copyright © 2018 Gabriel Cavalcante. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
